d876535ace9085563661335b32b23620408e35b1
Bob2156 <152450330+Bob2156@users.noreply.github.com>
2024-12-28 13:12:59 -0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,86 +1,83 @@
 const {
-	InteractionResponseType,
-	InteractionType,
-	verifyKey,
+    InteractionResponseType,
+    InteractionType,
+    verifyKey,
 } = require("discord-interactions");
 const getRawBody = require("raw-body");
- 
+
 const INVITE_COMMAND = {
-	name: "Invite",
-	description: "Get an invite link to add the bot to your server",
+    name: "invite",
+    description: "Get an invite link to add the bot to your server",
 };
- 
+
 const HI_COMMAND = {
-	name: "Hi",
-	description: "Say hello!",
+    name: "hi",
+    description: "Say hello!",
 };
- 
+
 const INVITE_URL = `https://discord.com/oauth2/authorize?client_id=${process.env.APPLICATION_ID}&scope=applications.commands`;
- 
-/**
- * Gotta see someone 'bout a trout
- * @param {VercelRequest} request
- * @param {VercelResponse} response
- */
+
 module.exports = async (request, response) => {
-	// Only respond to POST requests
-	if (request.method === "POST") {
-		// Verify the request
-		const signature = request.headers["x-signature-ed25519"];
-		const timestamp = request.headers["x-signature-timestamp"];
-		const rawBody = await getRawBody(request);
- 
-		const isValidRequest = verifyKey(
-			rawBody,
-			signature,
-			timestamp,
-			process.env.PUBLIC_KEY,
-		);
- 
-		if (!isValidRequest) {
-			console.error("Invalid Request");
-			return response.status(401).send({ error: "Bad request signature " });
-		}
- 
-		// Handle the request
-		const message = request.body;
- 
-		// Handle PINGs from Discord
-		if (message.type === InteractionType.PING) {
-			console.log("Handling Ping request");
-			response.send({
-				type: InteractionResponseType.PONG,
-			});
-		} else if (message.type === InteractionType.APPLICATION_COMMAND) {
-			// Handle our Slash Commands
-			switch (message.data.name.toLowerCase()) {
-				case SLAP_COMMAND.name.toLowerCase():
-					response.status(200).send({
-						type: 4,
-						data: {
-							content: "Hello!",
-						},
-					});
-					console.log("Slap Request");
-					break;
-				case INVITE_COMMAND.name.toLowerCase():
-					response.status(200).send({
-						type: 4,
-						data: {
-							content: INVITE_URL,
-							flags: 64,
-						},
-					});
-					console.log("Invite request");
-					break;
-				default:
-					console.error("Unknown Command");
-					response.status(400).send({ error: "Unknown Type" });
-					break;
-			}
-		} else {
-			console.error("Unknown Type");
-			response.status(400).send({ error: "Unknown Type" });
-		}
-	}
+    // Only respond to POST requests
+    if (request.method === "POST") {
+        // Verify the request
+        const signature = request.headers["x-signature-ed25519"];
+        const timestamp = request.headers["x-signature-timestamp"];
+        const rawBody = await getRawBody(request);
+
+        const isValidRequest = verifyKey(
+            rawBody,
+            signature,
+            timestamp,
+            process.env.PUBLIC_KEY,
+        );
+
+        if (!isValidRequest) {
+            console.error("Invalid Request");
+            return response.status(401).send({ error: "Bad request signature" });
+        }
+
+        // Handle the request
+        const message = JSON.parse(rawBody);
+
+        // Handle PINGs from Discord
+        if (message.type === InteractionType.PING) {
+            console.log("Handling Ping request");
+            response.send({
+                type: InteractionResponseType.PONG,
+            });
+        } else if (message.type === InteractionType.APPLICATION_COMMAND) {
+            // Handle our Slash Commands
+            switch (message.data.name.toLowerCase()) {
+                case INVITE_COMMAND.name.toLowerCase():
+                    response.status(200).send({
+                        type: 4,
+                        data: {
+                            content: INVITE_URL,
+                            flags: 64,
+                        },
+                    });
+                    console.log("Invite request");
+                    break;
+                case HI_COMMAND.name.toLowerCase():
+                    response.status(200).send({
+                        type: 4,
+                        data: {
+                            content: "Hello!",
+                        },
+                    });
+                    console.log("Hi request");
+                    break;
+                default:
+                    console.error("Unknown Command");
+                    response.status(400).send({ error: "Unknown Command" });
+                    break;
+            }
+        } else {
+            console.error("Unknown Type");
+            response.status(400).send({ error: "Unknown Type" });
+        }
+    } else {
+        response.status(405).send({ error: "Method Not Allowed" });
+    }
 };
